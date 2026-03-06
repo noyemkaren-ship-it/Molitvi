0f86523cc7c90bffceab136b70bcc20547ccab64
Haker <noyemkaren@gmail.com>
2026-03-07 01:22:22 +0300

    Hristos: 3v

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Request
 from starlette.templating import Jinja2Templates
 from hristos import mol, molitvi
-from iisus import info
+from iisus import info, info1
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -37,3 +37,39 @@ async def church(request: Request):
         "k": "☦",
         "info": info
     })
+
+@app.get("/earth_create1")
+async def earht_history(Request: Request):
+    return templates.TemplateResponse("earth.html", {
+        "request": Request,
+        "title": "1 история",
+        "zagalovor": "Создания земли",
+        "info1": info1[0]})
+
+
+@app.get("/earth_create2")
+async def earht_history2(Request: Request):
+    return templates.TemplateResponse("earth.html", {
+        "request": Request,
+        "title": "2 история",
+        "zagalovor": "Создания людей",
+        "info1": info1[1]})
+
+
+@app.get("/earth_create3")
+async def earht_history(Request: Request):
+    return templates.TemplateResponse("earth.html", {
+        "request": Request,
+        "title": "3 история",
+        "zagalovor": "Миссей",
+        "info1": info1[2]})
+
+
+@app.get("/isto")
+async def root(request: Request):
+    return templates.TemplateResponse(
+        "isto.html",
+        {
+            "request": request,
+            "k": "☦"
+        })
